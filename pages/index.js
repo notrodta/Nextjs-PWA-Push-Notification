bfc2788ec4dcfc32314745c7c5def0eb05abe094
Rod Ta <rody101@gmail.com>
2023-06-23 14:46:28 -0400

    Added if browser supports web push to UI

--- pages/index.js
+++ pages/index.js
@@ -8,6 +8,7 @@ export default function Home() {
   const [expirationTime, setExpirationTime] = useState("");
   const [keys, setKeys] = useState("");
   const [body, setBody] = useState("");
+  const [isSupportWebPush, setIsSupportWebPush] = useState("");
 
   const publicKey =
     "BDeEjWwSClAYzHE15bxl1I0vlnTryaLz8XrfiqpX_nq9sLnmrEL3W-q_y3628MGBjJ10XKFb21LKk1OQGBsrf9Q";
@@ -111,9 +112,22 @@ export default function Home() {
     }
   }
 
+  function currentBrowserSupportsPush() {
+    if (!"PushManager" in window) {
+      return false;
+    }
+    if (!"serviceWorker" in navigator) {
+      return false;
+    }
+    if (!"Notification" in window) {
+      return false;
+    }
+    return true;
+  }
+
   useEffect(() => {
-    console.log("use state");
-  }, []);
+    setIsSupportWebPush(currentBrowserSupportsPush());
+  });
 
   return (
     <div className="p-0">
@@ -129,6 +143,7 @@ export default function Home() {
         <p>keys (auth): {keys.auth}</p>
         <p>keys (p256dh): {keys.p256dh}</p>
         <p>response: {body}</p>
+        <p>Supports web push: {isSupportWebPush.toString()}</p>
         <div className="row">
           <Card src="https://images.pexels.com/photos/397096/pexels-photo-397096.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           <Card src="https://images.pexels.com/photos/629162/pexels-photo-629162.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
