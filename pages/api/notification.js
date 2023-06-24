5d4339b5b6b9f4d1665988470b1640e9dc055390
Rod Ta <rody101@gmail.com>
2023-06-23 23:49:09 -0400

    Update

--- pages/api/notification.js
+++ pages/api/notification.js
@@ -3,7 +3,7 @@ const webpush = require("web-push");
 const publicKey =
   "BDeEjWwSClAYzHE15bxl1I0vlnTryaLz8XrfiqpX_nq9sLnmrEL3W-q_y3628MGBjJ10XKFb21LKk1OQGBsrf9Q";
 const privateKey = "QhVLSPo2Ja5Dvgtp0qI9RFbEMVL0XTVzRnpOsXoMLgc";
-const webPushContact = "mailto: contact@my-site.com";
+const webPushContact = "mailto:contact@my-site.com";
 
 webpush.setVapidDetails(webPushContact, publicKey, privateKey);
 
