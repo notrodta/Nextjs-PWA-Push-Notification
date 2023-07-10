7be8066fc2e94d0f24e1ee55cbcf665fa396e569
Rod Ta <rody101@gmail.com>
2023-07-10 12:13:17 -0400

    clean up

--- pages/index.js
+++ pages/index.js
@@ -170,7 +170,7 @@ export default function Home() {
 
   return (
     <div className="p-0">
-      <Navbar />
+      {/* <Navbar /> */}
       {/* <div className="w-100">
         <img src="/home.jpg" className="img-fluid" />
       </div> */}
@@ -183,23 +183,23 @@ export default function Home() {
         <p>keys (auth): {keys.auth}</p>
         <p>keys (p256dh): {keys.p256dh}</p>
         <p>response: {body}</p>
-        <p>Supports web push: {isSupportWebPush.toString()}</p>
+        {/* <p>Supports web push: {isSupportWebPush.toString()}</p>
         <p>then res: {thenRes}</p>
         <p>errorRes: {errorRes}</p>
-        <p>clientError???: {clientError}</p>
+        <p>clientError???: {clientError}</p> */}
         {/* <p>clientError???: {clientError}</p> */}
-        <div className="row">
+        {/* <div className="row">
           <Card src="https://images.pexels.com/photos/397096/pexels-photo-397096.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           <Card src="https://images.pexels.com/photos/629162/pexels-photo-629162.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           <Card src="https://images.pexels.com/photos/6992/forest-trees-northwestisbest-exploress.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           <Card src="https://images.pexels.com/photos/302804/pexels-photo-302804.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           <Card src="https://images.pexels.com/photos/167698/pexels-photo-167698.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
           <Card src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?auto=compress&cs=tinysrgb&dpr=2&w=500" />
-        </div>
+        </div> */}
       </div>
-      <div className="py-3 text-white text-center w-100 bg-dark">
+      {/* <div className="py-3 text-white text-center w-100 bg-dark">
         <p>A PWA Web App built on Next.js</p>
-      </div>
+      </div> */}
     </div>
   );
 }
